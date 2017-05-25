2ead8cee3d07ca14cc916c56f1002a989d3a979d
sandri4 <pletin1997@outlook.com>
2017-05-25 14:00:04 +0300

    add posts

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,3 @@
+$(document).on('turbolinks:load', function(){
+ setTimeout("$('.alert').fadeOut('slow')", 3000)
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'home#show'
 
   devise_for :users
+
   get 'home/show'
+  resources :posts
 end
